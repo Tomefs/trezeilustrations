6eebfcca4562ea41a27f042ecabd00adafe16777
Tomé Simões <tomefsimoes@gmail.com>
2024-09-30 11:42:51 +0100

    files

---
+++ app.js
@@ -0,0 +1,19 @@
+window.addEventListener('scroll', reveal);
+
+function reveal(){
+    var reveals = document.querySelectorAll('.sobre');
+
+    for(var i = 0; i < reveals.length; i++){
+
+    var windowheight = window.innerHeight;
+    var revealtop = reveals[i].getBoundingClientRect().top;
+    var revealpoint = windowheight/2;
+
+    if(revealtop < windowheight - revealpoint){
+        reveals[i].classList.add('active');
+    }
+    else{
+        reveals[i].classList.remove('active');
+    }
+    } 
+}
